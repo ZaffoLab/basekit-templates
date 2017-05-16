2bd96fcf4f1fcbd9129f61734adedfc4fba857bb
gareth <gareth@clearandfizzy.com>
2017-05-16 16:15:21 +0100

    Initial commit  -- Initial commit of file system

---
+++ default/js/global.js
@@ -0,0 +1,11 @@
+    
+    // // fixes a image height bug
+    // jQuery( document ).ready(function() {
+    //     alert("#featured-hero-image .js-image");
+        
+    //     var featured_img = jQuery("#featured-hero-image .js-image");
+        
+    //     featured_img.css('display', 'block');
+    //     featured_img.css('visibility', 'hidden');
+    //     featured_img.appendTo('.feature-bgimg .content');
+    // });     
